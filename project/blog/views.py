a586f28688ad187aff6b86359ca3e03a5a51ed59
partha2000 <parthaprateem2000@gmail.com>
2020-10-18 01:25:43 +0530

    added most of the features

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Posts
 # Create your views here.
@@ -26,6 +29,49 @@ def home(request):
 	}
 	return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+	model = Posts
+	template_name = 'blog/home.html'  ## Django will be looking for <app>/<model>_<viewtype>.html
+	context_object_name = 'posts'
+	ordering = ['-date_posted'] ## Reverse chrono order
+
+
+class PostDetailView(DetailView):
+	model = Posts
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+	model = Posts
+	fields = ['title','content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin,  UpdateView):
+	model = Posts
+	fields = ['title','content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+	
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		else:
+			return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = Posts
+	success_url = ''
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+	
+	
 
 def about(request):
 	# return render(request, 'blog/about.html')
--- project/users/views.py
+++ project/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegistrationForm
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
@@ -11,8 +11,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('Username')
-            messages.success(request,f'Account created for {username}!') ## Alert message
-            return redirect('blog-home')  ## Redirect to home
+            messages.success(request,f'Account created for {username}! You are now able to login') ## Alert message
+            return redirect('login')  ## Redirect to blog home page
     else:
         form = UserRegistrationForm()   ## Blank form if no data
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
